d7b2750d94f3ff7fad558113743f19750dbcd67b
unknown <antony09.al.r2d2@gmail.com>
2024-09-20 00:43:43 -0600

    Primeras configuraciones de Java con Spring

---
+++ src/main/java/com/pokemon/pokemonApi/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.pokemon.pokemonApi.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer; // Importar Customizer
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults()); // Usar autenticación básica
+
+        // Deshabilitar CSRF solo si es necesario
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
